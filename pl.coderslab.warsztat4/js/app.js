412daf4f08b68ac7ea7af93fafcf27802d919f91
Kruchyxe <ragamuffin@o2.pl>
2021-01-17 19:59:27 +0100

    forth commit

--- pl.coderslab.warsztat4/js/app.js
+++ pl.coderslab.warsztat4/js/app.js
@@ -4,5 +4,19 @@ const apihost = 'https://todo-api.coderslab.pl';
 
 
 document.addEventListener('DOMContentLoaded', function() {
-
+    function apiListTasks() {
+        return fetch(
+            apihost + '/api/tasks',
+            {
+                headers: { Authorization: apikey }
+            }
+        ).then(
+            function (resp) {
+                if(!resp.ok) {
+                    alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
+                }
+                return resp.json();
+            }
+        )
+    }
 });
