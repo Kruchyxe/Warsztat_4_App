f7d7924c1e9fb9638c01eaa43f752cee500829ec
Kruchyxe <ragamuffin@o2.pl>
2021-01-23 12:10:44 +0100

    day 3 commit 2

--- pl.coderslab.warsztat4/js/script.js
+++ pl.coderslab.warsztat4/js/script.js
@@ -1,9 +1,6 @@
 const apikey = 'cd9b2a65-cb4b-4d0f-9102-42d836ccfdd5';
 const apihost = 'https://todo-api.coderslab.pl';
 
-// if(apikey == '') {
-//     alert('Wejdź na https://todo-api.coderslab.pl/apikey/create oraz skopiuj token i wklej do stałej "apikey", w pliku script.js. Inaczej nic się nie uda!');
-// }
 
 function apiListTasks() {
     return fetch(
@@ -20,7 +17,6 @@ function apiListTasks() {
         }
     )
 }
-
 
 function renderTask(taskId, title, description, status) {
     const section = document.createElement('section');
@@ -46,11 +42,22 @@ function renderTask(taskId, title, description, status) {
     const headerRightDiv = document.createElement('div');
     headerDiv.appendChild(headerRightDiv);
 
+    // przyciski widzoczne jezelis status zadania jest otwarty
+
     if (status == 'open') {
         const finishButton = document.createElement('button');
         finishButton.className = 'btn btn-dark btn-sm js-task-open-only';
         finishButton.innerText = 'Zakończ';
         headerRightDiv.appendChild(finishButton);
+
+    //    stworzenie Listener dla przycisku //finishButton//
+        //sprawdzić działanie
+        finishButton.addEventListener('click', function() {
+            apiUpdateTask(taskId, title, description, 'closed');
+            section.querySelectorAll('.js-task-open-only').forEach(
+                function(element) { element.parentElement.removeChild(element); }
+            );
+        });
     }
 
     const deleteButton = document.createElement('button');
@@ -78,7 +85,7 @@ function renderTask(taskId, title, description, status) {
             )
         }
     )
-
+    // przyciski widzoczne jezelis status zadania jest otwarty
     if (status == 'open') {
         const addOperationDiv = document.createElement('div');
         addOperationDiv.className = 'card-body js-task-open-only';
@@ -132,7 +139,6 @@ function apiListOperationsForTask(taskId) {
     );
 }
 
-// zamiana operationsList na ul
 function renderOperation(ul, status, operationId, operationDescription, timeSpent) {
     const li = document.createElement('li');
     li.className = 'list-group-item d-flex justify-content-between align-items-center';
@@ -144,6 +150,8 @@ function renderOperation(ul, status, operationId, operationDescription, timeSpen
     descriptionDiv.innerText = operationDescription;
     li.appendChild(descriptionDiv);
 
+
+    //sprawdzić timeSpent !!!!
     const time = document.createElement('span');
     time.className = 'badge badge-success badge-pill ml-2';
     time.innerText = formatTime(timeSpent);
@@ -154,23 +162,55 @@ function renderOperation(ul, status, operationId, operationDescription, timeSpen
         controlDiv.className = 'js-task-open-only'; // skąd ta klasa ?
         li.appendChild(controlDiv);
 
+        /*
+        * Stworzenie przycisku /addQuarterBtn/
+        */
+
         const addQuarterBtn = document.createElement('div');
         addQuarterBtn.className = "btn btn-outline-success btn-sm mr-2";
         addQuarterBtn.innerText = 'dodaj 15 min';
         controlDiv.appendChild(addQuarterBtn);
+
+        /*
+        * Dodanie Listener "klik" do przycisku /addQuarterBtn/
+        */
+
+        addQuarterBtn.addEventListener('click', function (){
+            apiUpdateOperation(operationId, operationDescription, timeSpent + 15)
+                .then(function (response){
+                    timeSpent.innerText = formatTime(response.data.timeSpent);
+                    timeSpent = response.data.timeSpent;
+                });
+        });
+
+        /*
+        * Stworzenie przycisku /addHourBtn/
+        */
 
         const addHourBtn = document.createElement('div');
         addHourBtn.className = "btn btn-outline-success btn-sm mr-2";
         addHourBtn.innerText = 'dodaj 1 h';
         controlDiv.appendChild(addHourBtn);
 
-    }
+        /*
+        * Dodanie Listener "klik" do przycisku /addHourBtn/
+        */
 
+        addHourBtn.addEventListener('click', function (){
+            apiUpdateOperation(operationId,operationDescription, timeSpent + 60)
+                //dlaczego then???
+                .then(function (response){
+                    timeSpent.innerText = formatTime(response.data.timeSpent);
+                    timeSpent = response.data.timeSpent;
+                });
+        });
+    }
 }
 
-function formatTime(timeSpent) {
-    const hours = Math.floor(timeSpent / 60);
-    const minutes = timeSpent % 60;
+//wykorzystanie podpowiedzi jak formatować czas
+function formatTime(total) {
+    const hours = Math.floor(total / 60);
+    const minutes = total % 60;
     if (hours > 0) {
         return hours + 'h ' + minutes + 'm';
     } else {
@@ -195,7 +235,7 @@ function apiCreateTask(title, description) {
         }
     )
 }
-
+// kasowanie zdania
 function apiDeleteTask(taskId) {
     return fetch(
         apihost + '/api/tasks/' + taskId,
@@ -213,6 +253,9 @@ function apiDeleteTask(taskId) {
     )
 }
 
+/*
+* stworzenie operacji zadań
+*/
 function apiCreateOperationForTask(taskId, description) {
     return fetch(
         apihost + '/api/tasks/' + taskId + '/operations',
@@ -230,9 +273,72 @@ function apiCreateOperationForTask(taskId, description) {
         }
     );
 }
+/*
+* zmiana operacji
+*/
+// analogicznie jak metoda POST // stworzenie metody PUT
+function apiUpdateOperation(operationId, description, timeSpent){
+    return fetch(
+        apihost + '/api/operations/' + operationId,
+        {
+            headers: { Authorization: apikey, 'Content-Type': 'application/json' },
+            body: JSON.stringify({description: description, timeSpent: timeSpent }),
+            method: 'PUT'
+        }
+    ).then(
+        function (res){
+            if(!res.ok) {
+                alert('Wystąpił błąd połączenia')
+            }
+            return res.json();
+        }
+    );
+}
+
+// zamykanie operacji
+function apiUpdateTask(taskId, title, description, status) {
+    return fetch(
+        apihost + '/api/tasks/' + taskId,
+        {
+            headers: { Authorization: apikey, 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title: title, description: description, status: status }),
+            method: 'PUT'
+        }
+    ).then(
+        function (resp) {
+            if(!resp.ok) {
+                alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
+            }
+            return resp.json();
+        }
+    );
+}
+
+function apiListAllTasks() {
+    return fetch(
+        apihost + '/api/tasks',
+        { headers: { 'Authorization': apikey } }
+    ).then(
+        function (resp) {
+            if(!resp.ok) {
+                alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
+            }
+            return resp.json();
+        }
+    );
+}
 
 
 document.addEventListener('DOMContentLoaded', function () {
+    apiListAllTasks().then(
+        function(response) {
+            response.data.forEach(
+                function(task) {
+                    renderTask(task.id, task.title, task.description, task.status);
+                }
+            )
+        }
+    );
     document.querySelector('.js-task-adding-form').addEventListener('submit', function (event) {
             event.preventDefault();
             // apiListTasks().then(
